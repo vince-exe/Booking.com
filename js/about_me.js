6af181c2e2cce024d85b2c5ca102933a55e996a2
Vincenzo Caliendo <82732029+vince-exe@users.noreply.github.com>
2023-07-23 16:28:16 +0200

    update
    updated: about_me.php/css/js

--- js/about_me.js
+++ js/about_me.js
@@ -1,6 +1,15 @@
+const firstNameBox = document.getElementById('firstname-box');
+const lastNameBox = document.getElementById('lastname-box');
+const emailBox = document.getElementById('email-box');
+
 fetch('../php_scripts/get_user.php').then(response => {
-    response.text().then(data => {
-        console.log(data)
+    response.json().then(data => {
+        if(data[0].status == "true") {
+            firstNameBox.placeholder = data[0].first_name;
+            lastNameBox.placeholder = data[0].last_name;
+            emailBox.placeholder = data[0].email;
+        }   
+        
     })
 })
 .catch(reason => {
